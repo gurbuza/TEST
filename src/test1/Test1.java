de13ecc30ccc184a180607a5d45a78d1052716b1
gurbuz <adem.gurbuz@iut-valence.fr>
2014-02-03 17:35:13 +0100

    I

---
+++ src/test1/Test1.java
@@ -0,0 +1,17 @@
+package test1;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        
+
+        if (args.length > 0)
+            System.out.println(args[0]);
+        else
+            System.out.println("ADEMAGE / Gurbuza !");
+        
+            
+    }
+
+}
